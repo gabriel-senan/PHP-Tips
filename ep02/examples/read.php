1474763f444d127ffe7703c88c67771d65e77f04
gabriel-senan <gabrielsnnh50@gmail.com>
2024-11-19 09:45:07 -0300

    acessando o db

---
+++ ep02/examples/read.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+// use CoffeeCode\DataLayer\Connect;
+
+// $conn = Connect::getInstance();
+
+// $error = Connect::getError();
+
+// if ($error) {
+//     echo $error->getMessage();
+//     die();
+// }
+
+// $query = $conn->query("SELECT * FROM actor");
+
+// var_dump($query->fetchAll());
